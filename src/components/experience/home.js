c0fb1e2b449e032c0e446c1086a5822a09ceda95
Robert Greenfield <robertgreenfield@Roberts-MacBook-Pro.local>
2016-05-30 16:27:20 -0400

    home feed using static data but needs to use dynamic data using listview and sample json from store folder currently using static data to show ui and ux

--- src/components/experience/home.js
+++ src/components/experience/home.js
@@ -48,8 +48,8 @@ module.exports = React.createClass({
 			});
 		*/
 		//USING SAMPLE DATA FROM STORE IN A DYNAMIC MANNER FOR now
-		var Entries = require('../stores/sampledata'); 
-		var Data = Entries.Entries;
+		//var Entries = require('../stores/sampledata');
+		//var Data = Entries.Entries;
 		/*this.setState({
 			dataSource : that.state.dataSource.cloneWithRows(Data),
 			isLoaded   : true,
@@ -57,6 +57,7 @@ module.exports = React.createClass({
 	},
 	//rendering component
 	render: function() {
+				/* FOR DYNAMIC DATA WHEN USING JSON FROM API
 				if (!this.state.isLoaded) {
 					this.fetchData();
           return this.renderLoadingView()
@@ -65,7 +66,34 @@ module.exports = React.createClass({
 					<View style={styles.container}>
 						{this.renderListView()}
 	    		</View>
-				);
+				); */
+				/* STATIC DATA TO SHOW UI/UX */
+				return <View>
+						<ArticlePreview
+							postTime={'30 minutes ago'}
+							entryBrand={'Blavity'}
+							key={'ICYMI: President Obama’s Town Hall #GunsInAmerica'}
+							src={{uri:'http://blavity.com/wp-content/uploads/2015/12/Blavity.png'}}
+							source={{uri: 'http://blavity.blavity.netdna-cdn.com/wp-content/uploads/2016/01/obama_gun_town_hall1-696x364.jpg?0fd4d3' }}
+							text={'ICYMI: President Obama’s Town Hall #GunsInAmerica'}
+							onPress={() => this.onArticleDetailsPress(entry)} />
+						<ArticlePreview
+							postTime={'1 hour ago'}
+							entryBrand={'Blavity'}
+							key={'First Look At Cast Of Nat Turner Biopic “The Birth Of A Nation”'}
+							src={{uri:'http://blavity.com/wp-content/uploads/2015/12/Blavity.png'}}
+							source={{uri: 'http://blavity.blavity.netdna-cdn.com/wp-content/uploads/2016/01/birth.jpg?0fd4d3' }}
+							text={'First Look At Cast Of Nat Turner Biopic “The Birth Of A Nation”'}
+							onPress={() => this.onArticleDetailsPress(entry)} />
+						<ArticlePreview
+							postTime={'30 minutes ago'}
+							entryBrand={'Blavity'}
+							key={'ICYMI: Presidemnt Obama’s Town Hall #GunsInAmerica'}
+							src={{uri:'http://blavity.com/wp-content/uploads/2015/12/Blavity.png'}}
+							source={{uri: 'http://blavity.blavity.netdna-cdn.com/wp-content/uploads/2016/01/obama_gun_town_hall1-218x150.jpg?0fd4d3' }}
+							text={'ICYMI: President Obama’s Town Hall #GunsInAmerica'}
+							onPress={() => this.onArticleDetailsPress(entry)} />
+					</View>
 	},
 	//loading render
 	renderLoadingView: function() {
